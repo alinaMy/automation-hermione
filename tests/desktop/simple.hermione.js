7520621fce9b2b048aed6042127d313d28bcffb5
artsenius <34055997+artsenius@users.noreply.github.com>
2018-03-10 13:33:28 -0800

    added first simple test case

--- tests/desktop/simple.hermione.js
+++ tests/desktop/simple.hermione.js
@@ -1,12 +1,12 @@
 var assert = require('chai').assert;
 
-describe('github', function() {
-  it('should find hermione', function() {
+describe('BioCarCare', function() {
+  it('exists', function() {
     return this.browser
-      .url('https://github.com/gemini-testing/hermione')
-      .getText('#readme h1')
+      .url('https://app.biocarcare.com/')
+      .getText('#react-root h3')
       .then(function(title) {
-        assert.equal(title, 'Hermione')
+        assert.equal(title, 'Choice a garage')
       });
   });
 });
